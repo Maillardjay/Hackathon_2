f9ed5839a70552e23d53958b8295f5da1f568b53
FLORIANE BERTHIER <floriane.berthier96@gmail.com>
2023-06-28 14:18:57 +0200

    header navbar

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import logo from "../assets/logo.png";
+
+function Header() {
+  return (
+    <div>
+      <nav>
+        <div className="flex flex-wrap items-center justify-between mx-auto p-2">
+          <img
+            src={logo}
+            className="w-40 mr-3 pt-3 pb-3"
+            alt="Emmaus connect logo"
+          />
+          <div className="md:block md:w-auto" id="navbar">
+            <ul className="flex flex-row font-medium p-2 mt-2 rounded bg-gray-50">
+              <li className="block py-2 pl-3 pr-2 text-text_color rounded hover:text-rose">
+                Ajouter un smartphone
+              </li>
+              <li className="block py-2 pl-3 pr-2 text-text_color rounded hover:text-rose">
+                FAQ
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div>
+        <hr className="border-t-2 border-dark_blue mr-6 ml-6 " />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      black: "#000000",
+      dark_blue: "#002743",
+      light_blue: "#00ACB0",
+      gray: "#EBEBEB",
+      yellow: "#F8C20D",
+      rose: "#E62460",
+      text_color: "#002743",
+    },
     extend: {},
   },
   plugins: [],
