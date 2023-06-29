e081eb2047b584e42f3bf759ac9ed494869c39d0
NicolasPanis <122783357+NicolasPanis@users.noreply.github.com>
2023-06-30 00:39:34 +0200

    Update Estimation.jsx

--- frontend/src/pages/Estimation.jsx
+++ frontend/src/pages/Estimation.jsx
@@ -2,12 +2,13 @@ import React, { useState, useEffect, Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 // eslint-disable-next-line import/no-extraneous-dependencies
+import { ToastContainer, toast } from "react-toastify";
+// eslint-disable-next-line import/no-extraneous-dependencies
 import "react-toastify/dist/ReactToastify.css";
 import Scene1 from "../Scene1";
 import Scene2 from "../Scene2";
 import Scene3 from "../Scene3";
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { ToastContainer, toast } from "react-toastify";
+
 
 
 function Estimation() {
