1a61efb7d338bf3ec991ba3ba2d06f88182bfb5a
Bnt-dev <bberanger.pro@gmail.com>
2023-06-29 09:44:52 +0200

    Mise en place de l'algo pour l'estimation du prix

--- frontend/src/pages/Estimation.jsx
+++ frontend/src/pages/Estimation.jsx
@@ -4,7 +4,6 @@ import { OrbitControls } from "@react-three/drei";
 import Scene from "../Scene";
 
 function Estimation() {
-  // const [phones, setPhones] = useState();
   const [brands, setBrands] = useState([]);
   const [models, setModels] = useState([]);
   const [networks, setNetworks] = useState([]);
@@ -14,12 +13,82 @@ function Estimation() {
   const [states, setStates] = useState([]);
   const [oss, setOss] = useState([]);
 
-  // const getPhones = () => {
-  //   fetch(`${import.meta.env.VITE_BACKEND_URL}/phone`)
-  //     .then((res) => res.json())
-  //     .then((data) => setPhones(data))
-  //     .catch((err) => console.error(err));
-  // };
+  const [phone, setPhone] = useState({
+    brand: "",
+    model: "",
+    os: "",
+    network: "",
+    storage: "",
+    ram: "",
+    screensize: "",
+    state: "",
+    IMEI: "",
+  });
+
+  const [price, setPrice] = useState(0);
+
+  const handlePhone = (name, value) => {
+    setPhone({ ...phone, [name]: value });
+  };
+
+  const isSalable = () => {
+    return phone.ram >= 6 && phone.storage >= 16 && phone.state !== "DEEE";
+  };
+
+  const estimatePrice = () => {
+    if (!isSalable()) {
+      console.info("non vendable");
+      return;
+    }
+
+    let valRam = 0;
+    let valStorage = 0;
+    let ponderation = 0;
+
+    if (phone.ram <= 6) {
+      valRam = 90;
+    } else if (phone.ram <= 8) {
+      valRam = 110;
+    } else if (phone.ram <= 12) {
+      valRam = 140;
+    } else if (phone.ram <= 16) {
+      valRam = 200;
+    } else {
+      valRam = 250;
+    }
+
+    if (phone.storage === 16) {
+      valStorage = 31;
+    } else if (phone.storage <= 32) {
+      valStorage = 45;
+    } else if (phone.storage <= 64) {
+      valStorage = 66;
+    } else if (phone.storage <= 128) {
+      valStorage = 80;
+    } else if (phone.storage <= 256) {
+      valStorage = 100;
+    } else if (phone.storage <= 512) {
+      valStorage = 120;
+    } else if (phone.storage <= 1000) {
+      valStorage = 150;
+    }
+
+    if (phone.state === "Réparable") {
+      ponderation = 0.5;
+    } else if (phone.state === "Bloqué") {
+      ponderation = 0.9;
+    } else if (phone.state === "Reconditionnable") {
+      ponderation = 0.95;
+    } else if (phone.state === "Reconditionné") {
+      ponderation = 1;
+    }
+
+    setPrice((valRam + valStorage) * ponderation);
+  };
+
+  useEffect(() => {
+    estimatePrice();
+  }, [phone]);
 
   const getBrands = () => {
     fetch(`${import.meta.env.VITE_BACKEND_URL}/brands`)
@@ -43,7 +112,7 @@ function Estimation() {
   };
 
   const getRam = () => {
-    fetch(`${import.meta.env.VITE_BACKEND_URL}/networks`)
+    fetch(`${import.meta.env.VITE_BACKEND_URL}/ram`)
       .then((res) => res.json())
       .then((data) => setRams(data))
       .catch((err) => console.error(err));
@@ -99,10 +168,17 @@ function Estimation() {
           <label htmlFor="Brand" className="flex flex-col font-semibold">
             {" "}
             Quelle est la marque de l'appareil ?
-            <select className="border rounded-md border-black h-10 mt-5 mb-5 pl-2 text-black ">
+            <select
+              className="border rounded-md border-black h-10 mt-5 mb-5 pl-2 text-black "
+              name="brand"
+              type="text"
+              onChange={(event) =>
+                handlePhone(event.target.name, event.target.value)
+              }
+            >
               <option className="opacity-50">Marque</option>
               {brands.map((brand) => (
-                <option key={brand.id} value={brand.id}>
+                <option key={brand.id} value={brand.name}>
                   {brand.name}
                 </option>
               ))}
@@ -110,10 +186,17 @@ function Estimation() {
           </label>
           <label htmlFor="Model" className="flex flex-col font-semibold">
             Quel est le modèle de l'appareil ?
-            <select className="border rounded-md border-black h-10 mt-5 mb-5 pl-2 text-black ">
+            <select
+              className="border rounded-md border-black h-10 mt-5 mb-5 pl-2 text-black "
+              name="model"
+              type="text"
+              onChange={(event) =>
+                handlePhone(event.target.name, event.target.value)
+              }
+            >
               <option value="">Modèle</option>
               {models.map((model) => (
-                <option key={model.id} value={model.id}>
+                <option key={model.id} value={model.name}>
                   {model.name}
                 </option>
               ))}
@@ -121,10 +204,17 @@ function Estimation() {
           </label>
           <label htmlFor="Model" className="flex flex-col font-semibold">
             Quel est le système d'exploitation de votre appareil ?
-            <select className="border rounded-md border-black h-10 mt-5 mb-5 pl-2 text-black ">
+            <select
+              className="border rounded-md border-black h-10 mt-5 mb-5 pl-2 text-black "
+              name="os"
+              type="text"
+              onChange={(event) =>
+                handlePhone(event.target.name, event.target.value)
+              }
+            >
               <option value="">Os</option>
               {oss.map((os) => (
-                <option key={os.id} value={os.id}>
+                <option key={os.id} value={os.name}>
                   {os.name}
                 </option>
               ))}
@@ -132,10 +222,17 @@ function Estimation() {
           </label>
           <label htmlFor="Model" className="flex flex-col font-semibold">
             Quel est le réseau de votre appareil ?
-            <select className="border rounded-md border-black h-10 mt-5 mb-5 pl-2 text-black ">
+            <select
+              className="border rounded-md border-black h-10 mt-5 mb-5 pl-2 text-black "
+              name="network"
+              type="text"
+              onChange={(event) =>
+                handlePhone(event.target.name, event.target.value)
+              }
+            >
               <option value="">Réseau</option>
               {networks.map((network) => (
-                <option key={network.id} value={network.id}>
+                <option key={network.id} value={network.name}>
                   {network.name}
                 </option>
               ))}
@@ -143,10 +240,17 @@ function Estimation() {
           </label>
           <label htmlFor="Storage" className="flex flex-col font-semibold">
             Quelle est sa capacité de stockage ?
-            <select className="border rounded-md border-black h-10 mt-5 mb-5 pl-2 text-black ">
+            <select
+              className="border rounded-md border-black h-10 mt-5 mb-5 pl-2 text-black "
+              name="storage"
+              type="text"
+              onChange={(event) =>
+                handlePhone(event.target.name, +event.target.value)
+              }
+            >
               <option value="">Stockage</option>
               {storages.map((storage) => (
-                <option key={storage.id} value={storage.id}>
+                <option key={storage.id} value={storage.capacity}>
                   {storage.capacity}
                 </option>
               ))}
@@ -154,10 +258,17 @@ function Estimation() {
           </label>
           <label htmlFor="RAM" className="flex flex-col font-semibold">
             De combien de Ram dispose t-il ?
-            <select className="border rounded-md border-black h-10 mt-5 mb-5 pl-2 text-black ">
+            <select
+              className="border rounded-md border-black h-10 mt-5 mb-5 pl-2 text-black "
+              name="ram"
+              type="text"
+              onChange={(event) =>
+                handlePhone(event.target.name, +event.target.value)
+              }
+            >
               <option value="">RAM</option>
               {rams.map((ram) => (
-                <option key={ram.id} value={ram.id}>
+                <option key={ram.id} value={ram.name}>
                   {ram.name}
                 </option>
               ))}
@@ -165,10 +276,17 @@ function Estimation() {
           </label>
           <label htmlFor="Screen" className="flex flex-col font-semibold">
             Quelle est la taille de l'écran ?
-            <select className="border rounded-md border-black h-10 mt-5 mb-5 pl-2 text-black ">
+            <select
+              className="border rounded-md border-black h-10 mt-5 mb-5 pl-2 text-black "
+              name="screensize"
+              type="text"
+              onChange={(event) =>
+                handlePhone(event.target.name, +event.target.value)
+              }
+            >
               <option>Taille de l'écran</option>
               {screensizes.map((screensize) => (
-                <option key={screensize.id} value={screensize.id}>
+                <option key={screensize.id} value={screensize.size}>
                   {screensize.size}
                 </option>
               ))}
@@ -176,9 +294,16 @@ function Estimation() {
           </label>
           <label htmlFor="State" className="flex flex-col font-semibold">
             Quel est l'état de l'appareil ?
-            <select className="border rounded-md border-black h-10 mt-5 mb-5 pl-2 text-black ">
+            <select
+              className="border rounded-md border-black h-10 mt-5 mb-5 pl-2 text-black "
+              name="state"
+              type="text"
+              onChange={(event) =>
+                handlePhone(event.target.name, event.target.value)
+              }
+            >
               {states.map((state) => (
-                <option key={state.id} value={state.id}>
+                <option key={state.id} value={state.name}>
                   {state.name}
                 </option>
               ))}
@@ -191,8 +316,13 @@ function Estimation() {
               type="text"
               className="border rounded-md border-cyan-700 h-10 mt-5 pl-2"
               placeholder="Renseigner l'IMEI de l'appareil"
-              name="name"
-              value=""
+              minLength={15}
+              maxLength={15}
+              name="IMEI"
+              value={phone.IMEI}
+              onChange={(event) =>
+                handlePhone(event.target.name, event.target.value)
+              }
             />
           </label>
 
@@ -225,10 +355,32 @@ function Estimation() {
             type="button"
             className="items-end ml-10 rounded-full bg-rose py-3 px-6 text-white"
             name="name"
+            value={price}
+            onClick={(event) => estimatePrice(event.target.value)}
           >
-            Estimation
+            Estimer
           </button>
         </div>
+        {isSalable() && phone.IMEI !== "" && (
+          <div className="flex justify-end pt-5 pb-5 pr-10 gap-10">
+            <button
+              type="button"
+              className="items-end ml-10 rounded-full bg-rose py-3 px-6 text-white"
+            >
+              Ajouter
+            </button>
+          </div>
+        )}
+        {!isSalable() && phone.IMEI !== "" && (
+          <div className="flex justify-end pt-5 pb-5 pr-10 gap-10">
+            <button
+              type="button"
+              className="items-end ml-10 rounded-full bg-rose py-3 px-6 text-white"
+            >
+              Recycler
+            </button>
+          </div>
+        )}
       </div>
       <div className="w-5/12">
         <Canvas>
