704797250ce5a3031e790f22d0f1517de4c62552
Jumzer <bluevictor@hotmail.fr>
2023-06-29 19:27:44 +0200

    navbar dynamique : affichage lien actif en rose

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import "./Header.css";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import logo from "../assets/logo.png";
-import { NavLink } from "react-router-dom";
 
 function Header() {
   return (
