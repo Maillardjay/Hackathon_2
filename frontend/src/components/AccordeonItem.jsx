c40bfd72486949dbcc9e7e77235d7ccb5828ff68
FLORIANE BERTHIER <floriane.berthier96@gmail.com>
2023-06-29 16:16:37 +0200

    update color

--- frontend/src/components/AccordeonItem.jsx
+++ frontend/src/components/AccordeonItem.jsx
@@ -9,17 +9,15 @@ function AccordionItem({ header, text }) {
     setActive(!active);
   };
   return (
-    <div className="shadow-md single-faq mb-8 w-full ml-4 mr-4 rounded-[20px] border border-gray bg-white lg:px-6 xl:px-8">
+    <div className="shadow-md mb-8 w-full ml-4 mr-4 rounded-[20px] border border-gray bg-white lg:px-6 xl:px-8">
       <button
         type="button"
         className="flex items-center w-full text-left"
         onClick={() => handleToggle()}
       >
-        <div className="mr-5 flex h-10 w-full max-w-[40px] items-center justify-center rounded-lg bg-primary bg-opacity-5 text-primary mt-4 mb-4">
+        <div className="mr-5 flex h-10 w-full max-w-[40px] items-center justify-center rounded-lg bg-opacity-5 mt-4 mb-4">
           <svg
-            className={`duration-200 ease-in-out fill-primary stroke-primary ${
-              active ? "rotate-180" : ""
-            }`}
+            className={`duration-200 ease-in-out${active ? "rotate-180" : ""}`}
             width="17"
             height="10"
             viewBox="0 0 17 10"
@@ -28,7 +26,6 @@ function AccordionItem({ header, text }) {
             <path
               d="M7.28687 8.43257L7.28679 8.43265L7.29496 8.43985C7.62576 8.73124 8.02464 8.86001 8.41472 8.86001C8.83092 8.86001 9.22376 8.69083 9.53447 8.41713L9.53454 8.41721L9.54184 8.41052L15.7631 2.70784L15.7691 2.70231L15.7749 2.69659C16.0981 2.38028 16.1985 1.80579 15.7981 1.41393C15.4803 1.1028 14.9167 1.00854 14.5249 1.38489L8.41472 7.00806L2.29995 1.38063L2.29151 1.37286L2.28271 1.36548C1.93092 1.07036 1.38469 1.06804 1.03129 1.41393L1.01755 1.42738L1.00488 1.44184C0.69687 1.79355 0.695778 2.34549 1.0545 2.69659L1.05999 2.70196L1.06565 2.70717L7.28687 8.43257Z"
               fill="#00ACB0"
-              className="color"
             />
           </svg>
         </div>
